29c08172beba10afd58ab2cdca7a2b4c768921f9
rijwan sakib <71920497+rijwansakib@users.noreply.github.com>
2023-08-21 08:09:48 +0600

    [DONE] Academic Department   service

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { UsersRoutes } from '../modules/user/user.route'
 import { semesterRoutes } from '../modules/academicSemester/academicSemester.route'
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/acdemicFaculty.routes'
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes'
 
 const router = express.Router()
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: '/academic-faculties',
     route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departmant',
+    route: AcademicDepartmentRoutes,
   },
 ]
 
